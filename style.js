3c59e963e2b22ec43a3afca4d0ff7ec7bb9b5768
geniusrks1 <105981384+geniusrks1@users.noreply.github.com>
2023-06-09 08:50:58 +0530

    Update style.js

---
+++ style.js
@@ -0,0 +1,22 @@
+*{
+    font-family: comic sans MS;
+}
+
+.container{
+    width: 75vw;
+    border: 2px solid black;
+    padding: 18px;
+    margin: 25px auto;
+}
+
+form{
+  text-align: center;
+}
+h1{
+    text-align: center;
+    font-size: 2rem;
+}
+
+input, button{
+    font-size: 1.2rem;
+}
